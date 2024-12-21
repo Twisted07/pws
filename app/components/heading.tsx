cdd2de55dfb7be4760199b00eea2dcc9ca0f36b3
Twisted07 <iamtwisted07@gmail.com>
2024-12-21 19:33:05 +0100

    update: line height

--- app/components/heading.tsx
+++ app/components/heading.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Heading = ({children, centered = false} : {children : React.ReactNode, centered?: boolean}) => {
 return (
-    <h1 className={`xl:text-5xl xl:mb-10 lg:mb-5 mb-3 text-3xl font-semibold leading-tight lg:font-bold lg:leading-snug ${centered && "text-center"}`}>{children}</h1>
+    <h1 className={`xl:text-5xl xl:mb-10 lg:mb-5 mb-3 text-3xl font-semibold leading-tight lg:font-bold lg:leading-snug xl:leading-[1.25] ${centered && "text-center"}`}>{children}</h1>
   )
 }
 
