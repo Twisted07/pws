c576131f6a37ca5ed52a69ab8a5083e57394fb13
Twisted07 <iamtwisted07@gmail.com>
2025-01-22 01:23:59 +0100

    create: contact us form with silent email submission functionality

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,8 +1,47 @@
-import React from 'react'
+"use client"
+import React, { useRef } from 'react'
+import emailjs from '@emailjs/browser';
 
 const ContactUsPage = () => {
+
+  const ref = useRef({} as HTMLFormElement);
+
+  function handleSubmit(e : any) {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      process.env.NEXT_PUBLIC_SERVICE_ID as string, // service ID
+      process.env.NEXT_PUBLIC_TEMPLATE_ID as string, // template ID
+      ref.current,
+      process.env.NEXT_PUBLIC_PUBLIC_KEY // public key
+    ).then (
+      () => {console.log("Success!")},
+      (err) => {console.error("Error: ", err)}
+    );
+  }
+  
   return (
-    <div>ContactUsPage</div>
+    <div>
+      <h1>Get in touch with us</h1>
+      <form ref={ref} onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="from_name" />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="reply_to" id="email" />
+        </div>
+
+        <div>
+          <label htmlFor="message">Message</label>
+          <textarea name="message" id="message" rows={20} cols={30}/>
+        </div>
+
+        <button>Submit</button>
+      </form>
+    </div>
   )
 }
 
